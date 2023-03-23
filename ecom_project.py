f2cccf40479f7dd28620cd4035b06e7950188cba
Scott <45835546+Reginld1408@users.noreply.github.com>
2023-03-23 00:20:27 -0600

    Update ecom_project.py

--- ecom_project.py
+++ ecom_project.py
@@ -2,14 +2,12 @@ import time
 from selenium.webdriver import ActionChains
 import random
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
 
-serv_obj = Service("C:/Users/sirwi/Downloads/chromedriver_win32/chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
-driver = webdriver.Chrome(service=serv_obj, options=options)
+driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 
 driver.get("http://tutorialsninja.com/demo/")
